548c0bad492462ec1c335ac56a0675da61ef0e2f
lucile40lpy <lucile.lapray2610@orange.fr>
2025-06-05 17:37:07 +0200

    share beginning of code

---
+++ re-analysis_25-06-05.R
@@ -0,0 +1,69 @@
+# ======== Re analysis of nestedness in empirical matrices ========
+
+## ==== 1. Library import ====
+
+library(tidyverse)
+library(progress)
+library(vegan)
+library(permute)
+library(lattice)
+
+options(mc.cores = max(2, parallel::detectCores() - 2))
+
+
+## ==== 2. Functions ====
+
+### A. Compute correlation ----
+compute_cor_coef <- function(mat) {
+  avg_inventory <- apply(mat, 2, function(item_col) {
+    mean(rowSums(mat)[item_col == 1])})
+  
+  item_stats <- data.frame(
+    Prevalence = colSums(mat),
+    AvgInventory = avg_inventory
+  )
+  return(cor(item_stats$Prevalence, item_stats$AvgInventory))
+}
+
+### B. Compute nodf p-value ----
+compute_p_val_nodf <- function(mat, b) {
+  tryCatch({
+    if(nrow(mat) > 1 && ncol(mat) > 1) {
+      out_nodf <- oecosimu(comm = mat, nestfun = nestednodf, 
+                           method = b, alt = "greater", nsimul = 1000,
+                           batchsize = 50, parallel = TRUE)
+      out_nodf$oecosimu$pval[3]
+    } else {
+      NA_real_
+    }
+  }, error = function(e) NA_real_)
+}
+
+### ---- C. Compute temp p-value ----
+compute_p_val_temp <- function(mat, b) {
+  tryCatch({
+    if(nrow(mat) > 1 && ncol(mat) > 1) {
+      out_temp <- oecosimu(comm = mat, nestfun = nestedtemp, 
+                           method = b, alt = "less", nsimul = 1000,
+                           batchsize = 50, parallel = TRUE)
+      out_temp$oecosimu$pval
+    } else {
+      NA_real_
+    }
+  }, error = function(e) NA_real_)
+}
+
+
+## ==== 3. Data import ====
+
+
+
+
+## ==== 4. Nestedness analysis ====
+
+### ---- A. Parameters lists ----
+metrics <- c('r0', 'r1', 'curveball')
+baselines <- c('r00', 'r0', 'r1', 'r2','c0','curveball', 'backtracking',
+               'swap', 'tswap','quasiswap', 'greedyqswap')
+
+
